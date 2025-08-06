c9f13a7ad1e7310ebb40ae65ff5992dc45471aab
tapos-chandro <taposchandro231@gmail.com>
2025-08-06 11:02:41 +0600

    first commit

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,26 @@
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function NavBar() {
+  const { data: session, status } = useSession();
+
+
+
+  if (status === "loading") return <p>Loading...</p>;
+
+
+
+
+  return (
+    <nav>
+      {session ? (
+        <>
+          <p>Hi, {session.user.name}</p>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </>
+      ) : (
+        <button onClick={() => signIn('google')} className="hover:cursor-pointer border-white bg-yellow-500 p-2 px-8 rounded-md">SignIn</button>
+      )}
+    </nav>
+  );
+}
